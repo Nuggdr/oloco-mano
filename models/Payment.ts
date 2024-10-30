7ec9e40a1f61b3cb8c4a3ca36058097fc392af3c
Nuggdr <nuggetmex003@outlook.com>
2024-10-29 21:07:55 -0300

    Update Payment.ts

--- models/Payment.ts
+++ models/Payment.ts
@@ -2,12 +2,18 @@ import mongoose from 'mongoose';
 
 const PaymentSchema = new mongoose.Schema({
   userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User', // Refira-se ao modelo do usuário
+  },
+  planId: {
     type: String,
     required: true,
   },
-  planId: {
-    type: Number,
+  paymentId: {
+    type: String,
     required: true,
+    unique: true, // Garanta que não haja duplicatas
   },
   paymentLink: {
     type: String,
@@ -15,8 +21,11 @@ const PaymentSchema = new mongoose.Schema({
   },
   status: {
     type: String,
+    enum: ['pending', 'approved', 'rejected'],
     default: 'pending',
   },
+}, {
+  timestamps: true,
 });
 
 export default mongoose.models.Payment || mongoose.model('Payment', PaymentSchema);
