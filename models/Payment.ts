496b8413baf8de07b46861c99f14fbf3e5d4d9da
Nuggdr <nuggetmex003@outlook.com>
2024-10-29 21:45:24 -0300

    Update Payment.ts

--- models/Payment.ts
+++ models/Payment.ts
@@ -4,6 +4,7 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IPayment extends Document {
   userId: string; // ID do usuário associado ao pagamento
   planId: number; // ID do plano
+  paymentId?: string; // ID do pagamento, se necessário
   status: string; // Status do pagamento
   date: Date; // Data do pagamento
   amount: number; // Valor do pagamento
@@ -12,6 +13,7 @@ export interface IPayment extends Document {
 const PaymentSchema: Schema = new Schema({
   userId: { type: String, required: true },
   planId: { type: Number, required: true },
+  paymentId: { type: String, unique: true, sparse: true }, // Adicione esta linha se necessário
   status: { type: String, required: true },
   date: { type: Date, default: Date.now },
   amount: { type: Number, required: true },
